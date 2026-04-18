e6efb7d713523b40e52032c70146ec16766b3e64
Musharraf-Bubere <musharrafbubere007@gmail.com>
2026-04-18 15:08:06 +0530

    SQL = basics

---
+++ SQL-MySQL/Practical_SQL_Query/01_employee_details_query.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE employee_details;
+USE employee_details;
--- SQL-MySQL/practical/01_titanic_datasets.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE titanic;
-
-USE titanic;
-
-SELECT * FROM titanic_dataset; -- All columns
-SELECT Name,Sex,Survived FROM titanic_dataset; -- Filter Columns
-SELECT Name AS PassengerName, SEX AS Gender, Survived AS Survived FROM titanic_dataset; -- Alias AS
